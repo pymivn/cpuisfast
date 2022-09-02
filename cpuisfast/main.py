99c9b0ba12524a03d129c42e3c36560bb2fc4e5a
Viet Hung Nguyen <hvn@familug.org>
2022-09-02 17:22:53 +0700

    Add static website

---
+++ cpuisfast/main.py
@@ -0,0 +1,25 @@
+import csv
+import json
+
+with open("data.csv") as f:
+    reader = csv.reader(f)
+    dr = csv.DictReader(
+        f,
+        fieldnames=[
+            "model",
+            "loop_per_second",
+            "add_per_second",
+            "system",
+            "arch",
+            "python_implementation",
+            "python_version",
+            "inserted_at",
+        ],
+    )
+    cpus = list(dr)
+    print(len(cpus))
+
+with open("template.html") as tpl, open("index.html", "wt") as out:
+    template = tpl.read()
+    rendered = template % (json.dumps(cpus),)
+    out.write(rendered)
